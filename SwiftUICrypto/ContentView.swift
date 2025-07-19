c38add3ae64189ee4cf1cbf05b42fcd74e1b9d6c
henriquebatochi <139269113+henriquebatochi@users.noreply.github.com>
2025-07-19 19:15:16 -0300

    Initial Commit

---
+++ SwiftUICrypto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUICrypto
+//
+//  Created by Henrique Alves Batochi on 19/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUICrypto/SwiftUICryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICryptoApp.swift
+//  SwiftUICrypto
+//
+//  Created by Henrique Alves Batochi on 19/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
