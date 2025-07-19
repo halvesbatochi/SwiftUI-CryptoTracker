82ca80ac953190c959de45cd1394f869886ece6a
henriquebatochi <139269113+henriquebatochi@users.noreply.github.com>
2025-07-19 20:30:15 -0300

    Init

---
+++ SwiftUICrypto/Extensions/Color.swift
@@ -0,0 +1,25 @@
+//
+//  Color.swift
+//  SwiftUICrypto
+//
+//  Created by Henrique Alves Batochi on 19/07/25.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
