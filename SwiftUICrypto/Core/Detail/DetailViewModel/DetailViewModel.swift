19cdc72d0371028b1b24ad04a84a98fa9473f020
Henrique Alves Batochi <henrique.alvesbatochi@gmail.com>
2025-11-01 23:35:25 -0300

    Display downloaded data

--- SwiftUICrypto/Core/Detail/DetailViewModel/DetailViewModel.swift
+++ SwiftUICrypto/Core/Detail/DetailViewModel/DetailViewModel.swift
@@ -10,19 +10,65 @@ import Combine
 
 class DetailViewModel: ObservableObject {
     
+    @Published var overviewStatistics: [StatisticModel] = []
+    @Published var additionalStatistics: [StatisticModel] = []
+    
+    @Published var coin: CoinModel
     private let coinDetailService: CoinDetailDataService
     private var cancellables = Set<AnyCancellable>()
     
     init(coin: CoinModel) {
+        self.coin = coin
         self.coinDetailService = CoinDetailDataService(coin: coin)
         addSubscribers()
     }
     
     private func addSubscribers() {
         coinDetailService.$coinDetails
-            .sink { (returnedCoinDetails) in
+            .combineLatest($coin)
+            .map({ (coinDetailModel, coinModel) -> (overview: [StatisticModel], additionalStatistics: [StatisticModel]) in
+
+                // overview
+                let price = coinModel.currentPrice.asCurrencyWith6Decimals()
+                let priceChange = coinModel.priceChangePercentage24H
+                let priceStat = StatisticModel(title: "Current Price", value: price, percentageChange: priceChange)
+                
+                let marketCap = "$" + (coinModel.marketCap?.formattedWithAbbreviations() ?? "")
+                let marketCapPercentChange = coinModel.marketCapChangePercentage24H
+                let marketCapStat = StatisticModel(title: "Market Capitalization", value: marketCap, percentageChange: marketCapPercentChange)
+                
+                let rank = "\(coinModel.rank)"
+                let rankStat = StatisticModel(title: "Rank", value: rank)
+                
+                let volume = "$" + (coinModel.totalVolume?.formattedWithAbbreviations() ?? "")
+                let volumeStat = StatisticModel(title: "Volume", value: volume)
+                
+                let overviewArray: [StatisticModel] = [
+                    priceStat, marketCapStat, rankStat, volumeStat
+                ]
+                
+                // additional
+                let high = coinModel.high24H?.asCurrencyWith6Decimals() ?? "n/a"
+                let highStat = StatisticModel(title: "24h High", value: high)
+                
+                let low = coinModel.low24H?.asCurrencyWith6Decimals() ?? "n/a"
+                let lowStat = StatisticModel(title: "24h Low", value: low)
+                
+                let priceChange2 = coinModel.priceChange24H?.asCurrencyWith6Decimals() ?? "n/a"
+                let pricePercentChange2 = coinModel.priceChangePercentage24H
+                let priceChangeStat = StatisticModel(title: "24h Price Change", value: priceChange2, percentageChange: pricePercentChange2)
+                
+                let marketCapChange = "$" + (coinModel.marketCapChangePercentage24H?.formattedWithAbbreviations() ?? "")
+                let marketCapPercentChange2 = coinModel.marketCapChangePercentage24H
+                let marketCapChangeStat = StatisticModel(title: "24h Market Cap Change", value: marketCapChange, percentageChange: marketCapPercentChange2)
+                
+                
+                
+                return (overviewArray, [])
+            })
+            .sink { (returnedArrays) in
                 print("RECEIVED COIN DETAIL DATA")
-                print(returnedCoinDetails?.id)
+                print(returnedArrays)
             }
             .store(in: &cancellables)
     }
--- SwiftUICrypto/Core/Detail/Views/DetailView.swift
+++ SwiftUICrypto/Core/Detail/Views/DetailView.swift
@@ -20,22 +20,67 @@ struct DetailLoadingView: View {
 
 struct DetailView: View {
     
-    @StateObject var vm: DetailViewModel
+    @StateObject private var vm: DetailViewModel
+    private let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    private let spacing: CGFloat = 30
     
     init(coin: CoinModel) {
         _vm = StateObject(wrappedValue: DetailViewModel(coin: coin))
-        print("Initializing Detail View for \(coin.name)")
     }
     
     var body: some View {
-        ZStack {
-            Text("Hello")
+        ScrollView {
+            VStack(spacing: 20) {
+                Text("hi")
+                    .frame(height: 150)
+                
+                Text("Overview")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(Color.theme.accent)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Divider()
+                
+                LazyVGrid(
+                    columns: columns,
+                    alignment: .leading,
+                    spacing: spacing,
+                    content: {
+                        ForEach(0..<6) { _ in
+                                StatisticView(stat: StatisticModel(title: "Title", value: "Value"))
+                        }
+                    })
+                
+                Text("Additional Details")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(Color.theme.accent)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Divider()
+                
+                LazyVGrid(
+                    columns: columns,
+                    alignment: .leading,
+                    spacing: spacing,
+                    content: {
+                        ForEach(0..<6) { _ in
+                                StatisticView(stat: StatisticModel(title: "Title", value: "Value"))
+                        }
+                    })
+            }
+            .padding()
         }
+        .navigationTitle(vm.coin.name)
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(coin: dev.coin)
+        NavigationView {
+            DetailView(coin: dev.coin)
+        }
     }
 }
