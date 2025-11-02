c836db01eea9aabbc84a4c4342fb7cdc729f13bd
Henrique Alves Batochi <henrique.alvesbatochi@gmail.com>
2025-11-02 16:45:17 -0300

    Adding final optimizations

--- SwiftUICrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftUICrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -154,7 +154,7 @@ class HomeViewModel: ObservableObject {
                                 }
                                 .reduce(0, +)
         
-        let percentageChange = ((portfolioValue - previousValue) / previousValue) * 100
+        let percentageChange = ((portfolioValue - previousValue) / previousValue)
         
         
         let portfolio = StatisticModel(title: "Portfolio Value",
--- SwiftUICrypto/Core/Home/Views/HomeView.swift
+++ SwiftUICrypto/Core/Home/Views/HomeView.swift
@@ -39,8 +39,14 @@ struct HomeView: View {
                     allCoinsList
                         .transition(.move(edge: .leading))
                 } else {
-                    portfolioCoinsList
-                        .transition(.move(edge: .trailing))
+                    ZStack(alignment: .top) {
+                        if vm.portfolioCoins.isEmpty && vm.searchText.isEmpty {
+                            portfolioEmptyText
+                        } else {
+                            portfolioCoinsList
+                        }
+                    }
+                    .transition(.move(edge: .trailing))
                         
                 }
                 
@@ -130,6 +136,15 @@ extension HomeView {
             }
         }
         .listStyle(PlainListStyle())
+    }
+    
+    private var portfolioEmptyText: some View {
+        Text("You haven't added any coins to your portfolio yet. Click the + button to get started!")
+            .font(.callout)
+            .foregroundColor(Color.theme.accent)
+            .fontWeight(.medium)
+            .multilineTextAlignment(.center)
+            .padding(50)
     }
     
     private func segue(coin: CoinModel) {
